bf3e6794ef64cf7777eae83c565096f2fb33a933
Ubuntu <ubuntu@ip-10-171-83-111.us-west-1.compute.internal>
2012-10-25 01:59:59 +0000

    Better way of doing the save in HW3.1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    new_movie = Movie.new(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
-    new_movie.save!
+   flunk "Moview not created" unless  Movie.create!(movie)
   end
 end
 
